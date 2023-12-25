d6fdd37355d35f0d500f2c54cc29471281d85125
Darshan <darshankotiann@gmail.com>
2023-12-25 18:32:12 +0530

    Logout Functioinality

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -72,7 +72,7 @@ const Sidebar = () => {
                         </ul>
                     </div>
                     <div className="p-8">
-                        <button type="button" class="flex align-middle focus:outline-none text-white bg-[#0064FF] hover:bg-[#4D70B6] focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                        <button type="button" onClick={()=>{localStorage.removeItem("adminToken");window.location.href="/"}} class="flex align-middle focus:outline-none text-white bg-[#0064FF] hover:bg-[#4D70B6] focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-4 lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline points="16 17 21 12 16 7" /><line x1="21" x2="9" y1="12" y2="12" /></svg>
                             Logout
                         </button>
