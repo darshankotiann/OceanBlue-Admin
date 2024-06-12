0b9dec226b36c992e91c8d8bdb122d68a20a4815
Mrudul Kolambe <mrudulkolambe02@gmail.com>
2024-06-13 00:35:18 +0530

    changes

--- src/app/notification/page.jsx
+++ src/app/notification/page.jsx
@@ -1,28 +1,92 @@
 "use client"
 
 import { useAuthContext } from '@/Context/AuthContext';
+import { useProductHistoryContext } from '@/Context/ProductHistory';
+import SearchableSelect from '@/Context/SearchOption';
+import { useVendorContext } from '@/Context/VendorContext';
 import Layout from '@/components/Layout/Layout';
 import Login from '@/components/Login';
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Notification = () => {
 	const { adminToken } = useAuthContext();
 	const [title, setTitle] = useState("");
 	const [body, setBody] = useState("");
+	const { vendorData } = useVendorContext();
+	const { productHistory } = useProductHistoryContext();
+	const [tokens, setTokens] = useState([])
+	const [selectedVendors, setSelectedVendors] = useState([])
+	const [listType, setListType] = useState("vendor")
+	const [list, setList] = useState([])
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		try {
-			const data = {
-				title, desc: body
-			}
-			axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/send`, data)
+		const data = {
+			title, desc: body, tokens
+		}
+		if (selectedVendors.length == 0) {
+			axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/send-all`, data)
 				.then((res) => {
 					alert("Notifications sent!")
 				})
-		} catch (error) {
-			alert("Network error!")
+		} else {
+			if (selectedVendors.length != tokens.length) {
+				alert(`${selectedVendors.length - tokens.length} have not permitted for notifications`)
+			}
+			try {
+				axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/send`, { ...data, tokens })
+					.then((res) => {
+						alert("Notifications sent!")
+					})
+			} catch (error) {
+				alert("Network error!")
+			}
+		}
+	}
+
+	const handleTokens = (vendors) => {
+		setSelectedVendors(vendors)
+		if (listType == "vendor") {
+			setTokens(vendors
+				.map(item => item.fcmtoken)
+				.filter(token => token !== '' && token !== undefined && token !== null))
+		} else {
+			setTokens(productHistory
+				.map(item => item.vendorID.fcmtoken)
+				.filter(token => token !== '' && token !== undefined && token !== null));
+		}
+	}
+
+	useEffect(() => {
+		setList(vendorData?.map((vendor) => {
+			return {
+				...vendor,
+				label: `${vendor.fullname} (#${vendor._id.slice(18)})`,
+				value: vendor._id
+			}
+		}))
+	}, [vendorData]);
+
+	const handleList = (e) => {
+		if (e == 'vendor') {
+			setListType("vendor");
+			setList(vendorData?.map((vendor) => {
+				return {
+					...vendor,
+					label: `${vendor.fullname} (#${vendor._id.slice(18)})`,
+					value: vendor._id
+				}
+			}))
+		} else {
+			setListType("order");
+			setList(productHistory?.map((order) => {
+				return {
+					...order,
+					label: `${order.vendorID.fullname} (#${order._id.slice(18)}) [${order.productID.category.brand} (${order.productID.category.name})]`,
+					value: vendor._id
+				}
+			}))
 		}
 	}
 	return (
@@ -34,7 +98,12 @@ const Notification = () => {
 					</div>
 					<div class="px-20 h-[75vh]">
 						<form onSubmit={handleSubmit} className='bg-white p-20 flex flex-col h-full' >
-							<label htmlFor="title" className='text-sm font-semibold'>Notification Title:</label>
+							<div className='flex gap-4 mb-3'>
+								<label htmlFor="vendor">Vendor <input onChange={() => handleList("vendor")} type="radio" name="type" id="vendor" /></label>
+								<label htmlFor="order">Order <input onChange={() => handleList("order")} type="radio" name="type" id="order" /></label>
+							</div>
+							<SearchableSelect onChange={handleTokens} id={"to"} isMulti={true} options={list} />
+							<label htmlFor="title" className='mt-4 text-sm font-semibold'>Notification Title:</label>
 							<input onChange={(e) => setTitle(e.target.value)} value={title} id='title' type="text" className="w-full py-3 bg-gray-50 text-gray-900 text-sm rounded-lg block  p-2 border-2 " placeholder="title" required />
 
 							<label htmlFor="body" className='mt-4 text-sm font-semibold'>Notification Description:</label>
